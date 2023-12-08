3e28a884cfd135f2941d446a20dcb91176725c20
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-12-07 21:09:02 -0700

    Gettings game options started

--- main.cpp
+++ main.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 using namespace std;
 
+
+class GameOptions {
+    public:
+        void options() {
+            cout << "Options: \n";
+            cout << "Show Options again: S\n";
+            cout << "Display Board:      D\n";
+            cout << "Edit Board:         E\n";
+            cout << "End Game:           G\n";
+        }
+
+        char getUserInput(char userOption) {
+            cout << ": ";
+            cin >> userOption;
+            
+            return userOption;
+        }
+};
+
+
+void gameRunner(){
+    GameOptions gameOptions;
+    char userOption;
+    userOption = gameOptions.getUserInput(userOption);
+
+
+
+    switch (toupper(userOption))
+    {
+    case 'S':
+        gameOptions.options();
+        break;
+    
+    case 'D':
+        break;
+
+    case 'E':
+        break;
+
+    case 'G':
+        break;
+
+    default:
+        break;
+    }
+
+}
+
 int main() {
-    cout << "Hello World";
+    GameOptions gO;
+    gO.options();
+
+
+    gameRunner();
+
     return 0;
 }
+
